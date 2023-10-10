0f1c06246f2d2de779c42547207f398383916c42
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-10-10 23:27:32 +0200

    patch update-entry

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,20 +1,30 @@
 import dbConnect from "../../../../db/connect";
+
 import Place from "../../../../db/models/place";
 
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET") {
-    const place = await Place.findById(id);
-    console.log("place:", place);
+  if (request.method === "PATCH") {
+    try {
+      const place = await Place.findById(id);
 
-    if (!place) {
-      return response.status(404).json({ status: "Not Found" });
+      if (!place) {
+        return response.status(404).json({ status: "Not Found" });
+      }
+
+      const updatedPlace = JSON.parse(request.body);
+      place.set(updatedPlace);
+      await place.save();
+
+      return response.status(200).json({ status: "Place updated" });
+    } catch (error) {
+      return response.status(500).json({ status: "Internal Server Error" });
     }
-
-    response.status(200).json({ place });
   }
+
+  response.status(405).end();
 
   // const comment = place?.comments;
   // const allCommentIds = comment?.map((comment) => comment.$oid) || [];
