efc1e9d3a09b1e24b31dc30f4865834f34aafbc8
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-10-11 14:14:08 +0200

    fix error for vercel

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,5 @@
 import dbConnect from "../../../db/connect";
-import Place from "../../../db/models/place";
+import Place from "../../../db/models/Place";
 
 export default async function handler(request, response) {
   await dbConnect();
